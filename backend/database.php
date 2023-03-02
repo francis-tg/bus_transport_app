53bbb6bbb570a233b3dc2e0931e1c96d4e3c6ee3
francis-tg <franc9885@gmail.com>
2023-03-02 11:44:59 +0000

    add trajet front and backend

--- backend/database.php
+++ backend/database.php
@@ -5,19 +5,18 @@ class database extends ORM
 {
     public function start()
     {
-        $this->createTable("relation_schema", [
+        $this->createTable("trajet", [
             'id' => 'INT(6) AUTO_INCREMENT PRIMARY KEY',
-            'table_name' => 'VARCHAR(30) NOT NULL',
-            'column_name' => 'VARCHAR(30) NOT NULL',
-            'related_table' => 'VARCHAR(30) NOT NULL',
-            'related_column' => 'VARCHAR(30) NOT NULL',
-            'createAt' => 'DATETIME(6) DEFAULT CURRENT_TIMESTAMP NOT NULL',
+            'id_depart' => 'INT(6) NOT NULL',
+            'id_dest' => 'INT(6) NOT NULL',
+            'prix' => 'VARCHAR(30) NOT NULL',
+            'createdAt' => 'DATETIME(6) DEFAULT CURRENT_TIMESTAMP NOT NULL',
         ]);
 
         $this->createTable("role", [
             'id' => 'INT(6) AUTO_INCREMENT PRIMARY KEY',
             'nom_role' => 'VARCHAR(30) NOT NULL',
-            'createAt' => 'DATETIME(6) DEFAULT CURRENT_TIMESTAMP NOT NULL',
+            'createdAt' => 'DATETIME(6) DEFAULT CURRENT_TIMESTAMP NOT NULL',
         ]);
         $this->createTable("user", [
             'id' => 'INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY',
@@ -32,13 +31,13 @@ class database extends ORM
             'id' => 'INT(6) AUTO_INCREMENT PRIMARY KEY',
             'ville' => 'VARCHAR(30) NOT NULL',
             'reached_time' => 'VARCHAR(30) NOT NULL',
-            'createAt' => 'DATETIME(6) DEFAULT CURRENT_TIMESTAMP NOT NULL',
+            'createdAt' => 'DATETIME(6) DEFAULT CURRENT_TIMESTAMP NOT NULL',
         ]);
         $this->createTable("depart", [
             'id' => 'INT(6) AUTO_INCREMENT PRIMARY KEY',
             'ville' => 'VARCHAR(30) NOT NULL',
             'leave_time' => 'VARCHAR(30) NOT NULL',
-            'createAt' => 'DATETIME(6) DEFAULT CURRENT_TIMESTAMP NOT NULL',
+            'createdAt' => 'DATETIME(6) DEFAULT CURRENT_TIMESTAMP NOT NULL',
         ]);
 
         $this->createTable("ticket", [
@@ -46,12 +45,15 @@ class database extends ORM
             'prix' => 'INT(10) NOT NULL',
             "nom_client" => "VARCHAR(60) NOT NULL",
             'phone' => 'VARCHAR(30)',
-            "id_depart" => 'INT(6)',
-            "id_arrive" => 'INT(6)',
-            'createAt' => 'DATETIME(6) DEFAULT CURRENT_TIMESTAMP NOT NULL',
+            "id_trajet" => 'INT(6)',
+            'createdAt' => 'DATETIME(6) DEFAULT CURRENT_TIMESTAMP NOT NULL',
         ]);
-        $this->createRelationship("ticket", "id_depart", "depart", "id");
-        $this->createRelationship("ticket", "id_arrive", "destination", "id");
+        
+        $this->createRelationship("ticket", "id_trajet", "trajet", "id");
+        $this->createRelationship("trajet", "id_dest", "destination", "id");
+        $this->createRelationship("trajet", "id_depart", "depart", "id");
+
+
 
         if (!$this->select("role", ["*"],"nom_role='administrateur'")) {
             $this->insert("role", [
--- src/Request/Destination.php
+++ src/Request/Destination.php
@@ -18,4 +18,21 @@ class Destination extends ORM
     }
 
     }
+    public function createTrajet(array $data){
+        if(isset($data["ville_depart"])&&isset($data["ville_arrive"])&&isset($data["prix"])){
+            extract($data);
+            $getDepart = $this->select("depart", ["*"], "ville='$ville_depart'", "id ASC", "1")[0];
+            $getDest = $this->select("destination", ["*"], "ville='$ville_arrive'", "id ASC", "1")[0];
+            $id_depart = $getDepart['id'];
+            $id_dest = $getDest['id'];
+            if(count($this->select("trajet",["*"], "id_depart='$id_depart'AND id_dest='$id_dest'"))===0){
+                $this->insert("trajet", [
+                    "id_depart" => $id_depart,
+                    "id_dest" => $id_dest,
+                    "prix" => $prix
+                ]);
+            }
+            header("Location: " . $_SERVER["HTTP_REFERER"]);
+        }
+    }
 }
