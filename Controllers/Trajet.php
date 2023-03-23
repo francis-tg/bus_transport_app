d98e73f4c25501ed1c0791a7569c8e5d18f46b60
francis-tg <franc9885@gmail.com>
2023-03-23 20:27:39 +0000

    secure route

--- Controllers/Trajet.php
+++ Controllers/Trajet.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace Cisco\Shadow\Controllers;
 
@@ -6,40 +6,58 @@ use Cisco\Shadow\Request\Destination;
 use Cisco\Shadow\Router;
 use Cisco\Shadow\View;
 
-class Trajet extends Destination{
-    function addDepart(array $data){
+class Trajet extends Destination
+{
+    public function addDepart(array $data)
+    {
         return $this->createDepart($data);
     }
-    function addDest(array $data){
+    public function addDest(array $data)
+    {
         return $this->addDestination($data);
     }
-    function Home(){
-        $villes_depart = $this->select("depart", ["*"]);
-        $villes_arrive = $this->select("destination", ["*"]);
-        $getTrajet = $this->select("trajet", ["*"],include:["depart" => "id_depart", "destination" => "id_dest"]);
-        View::render("admin/trajet",["title"=>"Admin | Trajets", 
-        "villes_depart"=>$villes_depart,
-        "villes_arrive"=>$villes_arrive,
-    "trajets"=>$getTrajet]);
+    public function Home()
+    {
+
+        if (isset($_SESSION["is_admin"]) && isset($_SESSION["user_id"])) {
+            $villes_depart = $this->select("depart", ["*"]);
+            $villes_arrive = $this->select("destination", ["*"]);
+            $getTrajet = $this->select("trajet", ["*"], include :["depart" => "id_depart", "destination" => "id_dest"]);
+            View::render("admin/trajet", [
+                "title" => "Admin | Trajets",
+                "villes_depart" => $villes_depart,
+                "villes_arrive" => $villes_arrive,
+                "trajets" => $getTrajet,
+            ]);
+        } else {
+            return Router::redirect("/login");
+        }
+
     }
-    function addTrajet(array $data){
+    public function addTrajet(array $data)
+    {
         return $this->createTrajet($data);
     }
-    function getTrajet(array $data)
+    public function getTrajet(array $data)
     {
-        $getTrajet = $this->select("trajet", ["*"], include: ["depart" => "id_depart", "destination" => "id_dest"]);
+
+        $getTrajet = $this->select("trajet", ["*"], include :["depart" => "id_depart", "destination" => "id_dest"]);
         return Router::json(200, $getTrajet);
+
     }
 
-    function getDepart(){
+    public function getDepart()
+    {
         return printf(json_encode($this->select("depart", ["*"])));
 
     }
-    function getArrive(){
+    public function getArrive()
+    {
         return printf(json_encode($this->select("destination", ["*"])));
     }
-    function getTrajetById(array $params){
-       $trajet = $this->selectOne("trajet", ["leave_ville","prix","dest_ville"], [["trajet.id" => $params["id"]]], include: ["depart" => "id_depart", "destination" => "id_dest"]);
+    public function getTrajetById(array $params)
+    {
+        $trajet = $this->selectOne("trajet", ["leave_ville", "prix", "dest_ville"], [["trajet.id" => $params["id"]]], include :["depart" => "id_depart", "destination" => "id_dest"]);
         return Router::json(200, $trajet);
     }
 }
